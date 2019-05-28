73708c7d638b00cbc00cc86cda7670932245f536
Stephan Hadinger <stephan.hadinger@gmail.com>
2019-05-28 17:27:34 +0200

    Added precompiled libbearssl with reduced footprint

--- sonoff/my_user_config.h
+++ sonoff/my_user_config.h
@@ -279,7 +279,7 @@
   #define WEB_PORT             80                // Web server Port for User and Admin mode
   #define WEB_USERNAME         "admin"           // Web server Admin mode user name
   #define USE_EMULATION_HUE                      // Enable Hue Bridge emulation for Alexa (+14k code, +2k mem common)
-  #define USE_EMULATION_WEMO                     // Enable Belkin WeMo emulation for Alexa (+6k code, +2k mem common)
+  //#define USE_EMULATION_WEMO                     // Enable Belkin WeMo emulation for Alexa (+6k code, +2k mem common)
 
 // -- mDNS ----------------------------------------
 //#define USE_DISCOVERY                            // Enable mDNS for the following services (+8k code, +0.3k mem)
