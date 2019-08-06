8ed93b1ee335026b3a36e28b86e39909c6a929ec
Stephan Hadinger <stephan.hadinger@gmail.com>
2019-08-06 22:04:52 +0200

    Names for Zigbee GPIO

--- sonoff/language/zh-TW.h
+++ sonoff/language/zh-TW.h
@@ -587,6 +587,8 @@
 #define D_SENSOR_ADE7953_IRQ   "ADE7953 IRQ"
 #define D_SENSOR_BUZZER        "Buzzer"
 #define D_SENSOR_OLED_RESET    "OLED Reset"
+#define D_SENSOR_ZIGBEE_TXD    "Zigbee Tx"
+#define D_SENSOR_ZIGBEE_RXD    "Zigbee Rx"
 
 // Units
 #define D_UNIT_AMPERE "安"
