cf6209326cb55bc3be6c8ec21f9ab16508a21fe8
Stephan Hadinger <stephan.hadinger@gmail.com>
2019-08-20 11:22:26 +0200

    Added Device Ids

--- sonoff/xdrv_23_zigbee_constants.ino
+++ sonoff/xdrv_23_zigbee_constants.ino
@@ -211,7 +211,7 @@ enum Z_Status {
   Z_BufferFull = 0x11
 };
 
-enum Z_App_profiles {
+enum Z_App_Profiles {
   Z_PROF_IPM = 0x0101,    // Industrial Plant Monitoring
   Z_PROF_HA = 0x0104,     // Home Automation -- the only supported right now
   Z_PROF_CBA = 0x0105,    // Commercial Building Automation
@@ -219,6 +219,43 @@ enum Z_App_profiles {
   Z_PROF_PHHC = 0x0108,   // Personal Home & Hospital Care
   Z_PROF_AMI = 0x0109,    // Advanced Metering Initiative
 };
+
+enum Z_Device_Ids {
+  Z_DEVID_CONF_TOOL = 0x0005,
+  // from https://www.rfwireless-world.com/Terminology/Zigbee-Profile-ID-list.html
+  // Generic	0x0000	ON/OFF Switch
+  // 0x0001	Level Control Switch
+  // 0x0002	ON/OFF Output
+  // 0x0003	Level Controllable Output
+  // 0x0004	Scene Selector
+  // 0x0005	Configuration Tool
+  // 0x0006	Remote control
+  // 0x0007	Combined Interface
+  // 0x0008	Range Extender
+  // 0x0009	Mains Power Outlet
+  // Lighting	0x0100	ON/OFF Light
+  // 0x0101	Dimmable Light
+  // 0x0102	Color Dimmable Light
+  // 0x0103	ON/OFF Light Switch
+  // 0x0104	Dimmer Switch
+  // 0x0105	Color Dimmer Switch
+  // 0x0106	Light Sensor
+  // 0x0107	Occupancy Sensor
+  // Closures	0x0200	Shade
+  // 0x0201	Shade Controller
+  // HVAC	0x0300	Heating/Cooling Unit
+  // 0x0301	Thermostat
+  // 0x0302	Temperature Sensor
+  // 0x0303	Pump
+  // 0x0304	Pump Controller
+  // 0x0305	Pressure Sensor
+  // 0x0306	Flow sensor
+  // Intruder Alarm Systems	0x0400	IAS Control and Indicating Equipment
+  // 0x0401	IAS Ancillary Control Equipment
+  // 0x0402	IAS Zone
+  // 0x0403	IAS Warning Device
+};
+
 //
 //
 //
