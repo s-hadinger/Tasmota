fc0cf788e13c6caca435618e92586ce6c8c3de06
Stephan Hadinger <stephan.hadinger@gmail.com>
2019-10-17 23:00:42 +0200

    Changed ZigbeeReceived format

--- sonoff/i18n.h
+++ sonoff/i18n.h
@@ -468,9 +468,13 @@
   #define D_JSON_ZIGBEEZNPSENT "ZigbeeZNPSent"
   #define D_JSON_ZIGBEEZCL_RECEIVED "ZigbeeZCLReceived"
   #define D_JSON_ZIGBEEZCL_RAW_RECEIVED "ZigbeeZCLRawReceived"
+  #define D_JSON_ZIGBEE_DEVICE "Device"
+  #define D_JSON_ZIGBEE_NAME "Name"
 #define D_CMND_ZIGBEE_ZCL_SEND "ZigbeeZCLSend"
   #define D_JSON_ZIGBEE_ZCL_SENT "ZigbeeZCLSent"
 #define D_CMND_ZIGBEE_PROBE "ZigbeeProbe"
+#define D_CMND_ZIGBEE_RECEIVED "ZigbeeReceived"
+  #define D_CMND_ZIGBEE_LINKQUALITY "LinkQuality"
 
   // Commands xdrv_25_A4988_Stepper.ino
   #ifdef USE_A4988_STEPPER
--- sonoff/xdrv_23_zigbee_8_parsers.ino
+++ sonoff/xdrv_23_zigbee_8_parsers.ino
@@ -385,7 +385,9 @@ int32_t Z_ReceiveAfIncomingMessage(int32_t res, const class SBuffer &buf) {
 
   DynamicJsonBuffer jsonBuffer;
   JsonObject& json_root = jsonBuffer.createObject();
-  JsonObject& json = json_root.createNestedObject(shortaddr);
+  JsonObject& json = json_root.createNestedObject(F(D_CMND_ZIGBEE_RECEIVED));
+  json[F(D_JSON_ZIGBEE_DEVICE)] = shortaddr;
+  // TODO add name field if it is known
   if ( (!zcl_received.isClusterSpecificCommand()) && (ZCL_REPORT_ATTRIBUTES == zcl_received.getCmdId())) {
    zcl_received.parseRawAttributes(json);
   } else if ( (!zcl_received.isClusterSpecificCommand()) && (ZCL_READ_ATTRIBUTES_RESPONSE == zcl_received.getCmdId())) {
@@ -399,11 +401,13 @@ int32_t Z_ReceiveAfIncomingMessage(int32_t res, const class SBuffer &buf) {
   AddLog_P2(LOG_LEVEL_INFO, PSTR("ZigbeeZCLRawReceived: %s"), msg.c_str());
 
   zcl_received.postProcessAttributes(srcaddr, json);
+  // Add linkquality
+  json[F(D_CMND_ZIGBEE_LINKQUALITY)] = linkquality;
 
   msg = "";
   json_root.printTo(msg);
   Response_P(PSTR("%s"), msg.c_str());
-  MqttPublishPrefixTopic_P(RESULT_OR_TELE, PSTR(D_JSON_ZIGBEEZCL_RECEIVED));
+  MqttPublishPrefixTopic_P(TELE, PSTR(D_RSLT_SENSOR));
   XdrvRulesProcess();
   return -1;
 }
