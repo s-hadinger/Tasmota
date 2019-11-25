b35cbfefd3590c9a0300c6c367fabf3d5811054b
Hadinger <hadinger@f45c89a46965.ant.amazon.com>
2019-11-25 16:43:42 +0100

    Reduce LightState - 28 bytes

--- tasmota/xdrv_04_light.ino
+++ tasmota/xdrv_04_light.ino
@@ -1363,6 +1363,8 @@ void LightPowerOn(void)
   }
 }
 
+const char LIGHT_POWER_DIMMER_STATUS[] PROGMEM = "\"" D_RSLT_POWER "%d\":\"%s\",\"" D_CMND_DIMMER "%d\":%d";
+
 void LightState(uint8_t append)
 {
   char scolor[LIGHT_COLOR_SIZE];
@@ -1377,10 +1379,16 @@ void LightState(uint8_t append)
   if (!Light.pwm_multi_channels) {
     if (unlinked) {
       // RGB and W are unlinked, we display the second Power/Dimmer
-      ResponseAppend_P(PSTR("\"" D_RSLT_POWER "%d\":\"%s\",\"" D_CMND_DIMMER "%d\":%d"
-                           ",\"" D_RSLT_POWER "%d\":\"%s\",\"" D_CMND_DIMMER "%d\":%d"),
-                            Light.device, GetStateText(Light.power & 1), Light.device, light_state.getDimmer(1),
+
+      ResponseAppend_P(LIGHT_POWER_DIMMER_STATUS,
+                            Light.device, GetStateText(Light.power & 1), Light.device, light_state.getDimmer(1));
+      ResponseAppend_P(PSTR("\""));
+      ResponseAppend_P(LIGHT_POWER_DIMMER_STATUS,
                             Light.device + 1, GetStateText(Light.power & 2 ? 1 : 0), Light.device + 1, light_state.getDimmer(2));
+      // ResponseAppend_P(PSTR("\"" D_RSLT_POWER "%d\":\"%s\",\"" D_CMND_DIMMER "%d\":%d"
+      //                      ",\"" D_RSLT_POWER "%d\":\"%s\",\"" D_CMND_DIMMER "%d\":%d"),
+      //                       Light.device, GetStateText(Light.power & 1), Light.device, light_state.getDimmer(1),
+      //                       Light.device + 1, GetStateText(Light.power & 2 ? 1 : 0), Light.device + 1, light_state.getDimmer(2));
     } else {
       GetPowerDevice(scommand, Light.device, sizeof(scommand), Settings.flag.device_index_enable);  // SetOption26 - Switch between POWER or POWER1
       ResponseAppend_P(PSTR("\"%s\":\"%s\",\"" D_CMND_DIMMER "\":%d"), scommand, GetStateText(Light.power & 1),
