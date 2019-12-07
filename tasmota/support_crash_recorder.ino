0b0252ba7b2b3330b32c38a7a1299cdbeb35fd97
Hadinger <hadinger@f45c89a46965.ant.amazon.com>
2019-12-07 10:35:20 +0100

    wip

--- tasmota/support_crash_recorder.ino
+++ tasmota/support_crash_recorder.ino
@@ -1,5 +1,5 @@
 /*
-  xdrv_23_crash_recorder.ino - record a complete stacktrace in Flash in case of crash
+  support_crash_recorder.ino - record the call stack in RTC in cas of crash
 
   Copyright (C) 2019  Stephan Hadinger, Theo Arends, 
 
@@ -17,39 +17,23 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define XDRV_32             32
-
-const char kCrashRecorderCommands[] PROGMEM = "|" "Crash|CrashDump"; // No prefix
-
-void (* const CrashRecorderCommand[])(void) PROGMEM = { &CmndCrash, &CmndCrashDump };
-
 const uint32_t dump_max_len = 64;                   // dump only 64 call addresses
 
-// typedef struct CrashRecorder_t {
-//   uint64_t crash_signature = crash_sig;
-//   uint32_t epc1;
-//   uint32_t stack_start;
-//   uint32_t stack_end;
-//   uint32_t stack_dump_len;
-//   uint32_t stack[];
-// } CrashRecorder_t;
-
 /**
- * Save crash information in Flash
+ * Save crash information in RTC memory
  * This function is called automatically if ESP8266 suffers an exception
  * It should be kept quick / consise to be able to execute before hardware wdt may kick in
  */
 extern "C" void custom_crash_callback(struct rst_info * rst_info, uint32_t stack, uint32_t stack_end ) {
-  uint32_t addr_written = 0;      // how many addresses have we written in RTC
-  uint32_t value;
+  uint32_t addr_written = 0;      // how many addresses have we already written in RTC
+  uint32_t value;                 // 4 bytes buffer to write to RTC
 
   for (uint32_t i = stack; i < stack_end; i += 4) {
-    value = *((uint32_t*) i);
-    if ((value >= 0x40000000) && (value < 0x40300000)) {
-      // valid address, we store it
+    value = *((uint32_t*) i);     // load value from stack
+    if ((value >= 0x40000000) && (value < 0x40300000)) {  // keep only addresses in code area
       ESP.rtcUserMemoryWrite(addr_written, (uint32_t*)&value, sizeof(value));
       addr_written++;
-      if (addr_written >= dump_max_len) { break; }
+      if (addr_written >= dump_max_len) { break; }        // we store only 64 addresses
     }
   }
   // fill the rest of RTC with zeros
@@ -57,20 +41,20 @@ extern "C" void custom_crash_callback(struct rst_info * rst_info, uint32_t stack
   while (addr_written < dump_max_len) {
     ESP.rtcUserMemoryWrite(addr_written++, (uint32_t*)&value, sizeof(value));
   }
-Serial.flush();
 }
 
-// Generate a crash to test the crash record
+// Generate a crash to test the crash recorder
 void CmndCrash(void)
 {
   if (1 == XdrvMailbox.payload) {
     volatile uint32_t dummy;
     dummy = *((uint32_t*) 0x00000000);                // invalid address
   } else {
-    ResponseCmndChar("1 to crash");
+    ResponseCmndChar(D_JSON_ONE_TO_CRASH);
   }
 }
 
+// Clear the RTC dump area when we do a normal reboot, this avoids garbage data to stay in RTC
 void CrashDumpClear(void) {
   uint32_t value = 0;
   for (uint32_t i = 0; i < dump_max_len; i++) {
@@ -79,12 +63,11 @@ void CrashDumpClear(void) {
 }
 
 /*********************************************************************************************\
- * CmndCrashDump - dump the crash history
+ * CmndCrashDump - dump the crash history - called by `Status 12`
 \*********************************************************************************************/
-
-void CmndCrashDump(void)
+void CrashDump(void)
 {
-  Response_P(PSTR("{\"call_chain\":["));
+  ResponseAppend_P(PSTR("{\"call_chain\":["));
   for (uint32_t i = 0; i < dump_max_len; i++) {
     uint32_t value;
     ESP.rtcUserMemoryRead(i, (uint32_t*)&value, sizeof(value));
@@ -94,17 +77,4 @@ void CmndCrashDump(void)
     }
   }
   ResponseAppend_P(PSTR("]}"));
-}
-
-/*********************************************************************************************\
- * Interface
-\*********************************************************************************************/
-
-bool Xdrv32(uint8_t function)
-{
-  if (FUNC_COMMAND == function) {
-    return DecodeCommand(kCrashRecorderCommands, CrashRecorderCommand);
-  } else {
-    return false;
-  }
 }
