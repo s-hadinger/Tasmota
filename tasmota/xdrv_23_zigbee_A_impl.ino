bdd6de2299813918b4c8f748877f42e2ca885852
Stephan Hadinger <stephan.hadinger@gmail.com>
2020-07-05 20:07:03 +0200

    EZSP CmndZbBindState

--- tasmota/xdrv_23_zigbee_A_impl.ino
+++ tasmota/xdrv_23_zigbee_A_impl.ino
@@ -767,6 +767,14 @@ void CmndZbBindState(void) {
   ZigbeeZNPSend(buf.getBuffer(), buf.len());
 #endif // USE_ZIGBEE_ZNP
 
+
+#ifdef USE_ZIGBEE_EZSP
+  // ZDO message payload (see Zigbee spec 2.4.3.3.4)
+  uint8_t buf[] = { 0x00 };           // index = 0
+
+  EZ_SendZDO(shortaddr, ZDO_Mgmt_NWK_Update_req, buf, sizeof(buf));
+#endif // USE_ZIGBEE_EZSP
+
   ResponseCmndDone();
 }
 
