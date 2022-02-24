a2aa087392b2856bc9d151135c12714a70ec723d
Stephan Hadinger <stephan.hadinger@gmail.com>
2022-02-24 11:47:25 +0100

    wip

--- lib/libesp32/berry/generate/be_fixed_lv_tasmota.h
+++ lib/libesp32/berry/generate/be_fixed_lv_tasmota.h
@@ -1,21 +1,23 @@
 #include "be_constobj.h"
 
 static be_define_const_map_slots(m_liblv_tasmota_map) {
-    { be_const_key(register_button_encoder, -1), be_const_func(lv0_register_button_encoder) },
-    { be_const_key(screenshot, 6), be_const_func(lv0_screenshot) },
-    { be_const_key(load_freetype_font, 8), be_const_func(lv0_load_freetype_font) },
-    { be_const_key(font_embedded, -1), be_const_func(lv0_load_font_embedded) },
-    { be_const_key(montserrat_font, -1), be_const_func(lv0_load_montserrat_font) },
-    { be_const_key(init, 7), be_const_closure(lv_tasmota_init_closure) },
     { be_const_key(start, -1), be_const_func(lv0_start) },
-    { be_const_key(font_seg7, -1), be_const_func(lv0_load_seg7_font) },
+    { be_const_key(screenshot, -1), be_const_func(lv0_screenshot) },
+    { be_const_key(register_button_encoder, -1), be_const_func(lv0_register_button_encoder) },
+    { be_const_key(init, -1), be_const_closure(lv_tasmota_init_closure) },
     { be_const_key(font_montserrat, -1), be_const_func(lv0_load_montserrat_font) },
+    { be_const_key(splash_init, -1), be_const_closure(lv_tasmota_splash_init_closure) },
+    { be_const_key(splash, 4), be_const_closure(lv_tasmota_splash_closure) },
+    { be_const_key(font_seg7, -1), be_const_func(lv0_load_seg7_font) },
+    { be_const_key(load_freetype_font, -1), be_const_func(lv0_load_freetype_font) },
     { be_const_key(seg7_font, -1), be_const_func(lv0_load_seg7_font) },
+    { be_const_key(montserrat_font, -1), be_const_func(lv0_load_montserrat_font) },
+    { be_const_key(font_embedded, 0), be_const_func(lv0_load_font_embedded) },
 };
 
 static be_define_const_map(
     m_liblv_tasmota_map,
-    10
+    12
 );
 
 static be_define_const_module(
